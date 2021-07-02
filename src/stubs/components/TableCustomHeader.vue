cc72ec4cc96b095369fb77a0f33813303e05b598
Tainaka2003 <di.mlik@yandex.ru>
2021-07-02 17:22:16 +0300

    sort

--- src/stubs/components/TableCustomHeader.vue
+++ src/stubs/components/TableCustomHeader.vue
@@ -1,12 +1,14 @@
 <template>
   <th>
-    <div :title="getMessageByKey(header.title)">
+    <div @click="sorting" :title="getMessageByKey(header.title) ">
       {{getMessageByKey(header.title)}}<br/>
     </div>
   </th>
 </template>
 
 <script>
+import {mapState} from "vuex";
+
 export default {
   name: 'TableCustomHeader',
 
@@ -16,6 +18,23 @@ export default {
       default() {
         return {};
       },
+    },
+    searched: Array
+  },
+  computed: {
+    ...mapState ([
+      'users',
+    ]),
+  },
+  methods: {
+    sorting() {
+      return this.users.sort(function (a, b) {
+        if (a.name < b.name)
+          return -1;
+        if (a.name > b.name)
+          return 1;
+        return 0;
+      })
     },
   },
 };
